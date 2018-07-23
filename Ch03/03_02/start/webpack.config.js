8dd0515301ccf461d8930679a3d9cb4ab8a3a5d3
call17 <GztwNI34>
2018-07-23 09:23:42 +0400

    complete 16 lesson

--- Ch03/03_02/start/webpack.config.js
+++ Ch03/03_02/start/webpack.config.js
@@ -1,47 +1,36 @@
 var webpack = require("webpack");
+const path = require('path');
 
 module.exports = {
-	entry: "./src/index.js",
-	output: {
-		path: "dist/assets",
-		filename: "bundle.js",
-		publicPath: "assets"
-	},
-	devServer: {
-		inline: true,
-		contentBase: './dist',
-		port: 3000
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				loader: ["babel-loader"],
-				query: {
-					presets: ["latest", "stage-0", "react"]
-				}
-			},
-			{
-				test: /\.json$/,
-				exclude: /(node_modules)/,
-				loader: "json-loader"
-			},
-			{
-				test: /\.css$/,
-				loader: 'style-loader!css-loader!autoprefixer-loader'
-			},
-			{
-				test: /\.scss$/,
-				loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
-			}
-		]
-	}
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist/assets"),
+        filename: "bundle.js",
+        publicPath: "assets"
+    },
+    devServer:{
+        inline: true,
+        contentBase: "./dist",
+        port:3001
+    },
+    module:{
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query:{
+                    presets:[ "latest", "stage-0", "react"]
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader'
+            },
+            {
+                test:/\.scss$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
+            }
+        ]
+    }
 }
-
-
-
-
-
-
-
