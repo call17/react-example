f0b0403ca4475e12f3f0ad5d91010629c0b3384d
Victor <victoor.yakimov@gmail.com>
2018-07-24 23:11:56 +0400

    complete 21 lesson

--- Ch04/04_01/start/src/components/SkiDayCount.js
+++ Ch04/04_01/start/src/components/SkiDayCount.js
@@ -1,7 +1,7 @@
 import '../stylesheets/ui.scss'
 import Terrain from 'react-icons/lib/md/terrain'
 import SnowFlake from 'react-icons/lib/ti/weather-snow'
-import Calendar from 'react-icons/lib/fa/calendar'
+import Calendar from 'react-icons/lib/ti/calendar'
 
 const percentToDecimal = (decimal) => {
 	return ((decimal * 100) + '%')
@@ -15,23 +15,23 @@ export const SkiDayCount = ({total, powder, backcountry, goal}) => (
 		<div className="ski-day-count">
 			<div className="total-days">
 				<span>{total}</span>
-					<Calendar />
+				<Calendar/>
 				<span>days</span>
 			</div>
 			<div className="powder-days">
 				<span>{powder}</span>
-					<SnowFlake />
+				<SnowFlake/>
 				<span>days</span>
 			</div>
 			<div className="backcountry-days">
 				<span>{backcountry}</span>
-					<Terrain />
-				<span>days</span>
+				<Terrain/>
+				<span>days</ span>
 			</div>
 			<div>
 				<span>
 					{calcGoalProgress(
-						total, 
+						total,
 						goal
 					)}
 				</span>
