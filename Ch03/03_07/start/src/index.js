b6766c9d502fc851f19e4bb2dfaf89e6aef15c7b
Victor <victoor.yakimov@gmail.com>
2018-07-31 23:44:42 +0400

    complete 27 lesson

--- Ch03/03_07/start/src/index.js
+++ Ch03/03_07/start/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayList } from './components/SkiDayList'
-import { SkiDayCount } from './components/SkiDayCount'
-
+import './stylesheets/ui.scss'
+import { App } from './components/App'
 
 window.React = React
 
@@ -13,27 +12,6 @@ window.React = React
 // )
 
 render(
-	<SkiDayList days={
-		[
-			{
-				resort: "Squaw Va lley",
-				date: new Date("1/2/2016"),
-				powder: true,
-				backcountry: false
-			},
-            {
-                resort: "Kirkwood",
-                date: new Date("3/29/2016"),
-                powder: false,
-                backcountry: false
-            },
-            {
-                resort: "Mt. Tallac",
-                date: new Date("4/2/2016"),
-                powder: false,
-                backcountry: true
-            }
-		]
-	}/>,
+	<App/>,
 	document.getElementById('react-container')
 )
