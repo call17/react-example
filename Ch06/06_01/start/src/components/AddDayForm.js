e743a89137dcf22e751e7568571f4472b742f54e
Якимов Виктор Игоревич <VIYakimov.SBT@sberbank.ru>
2018-08-02 14:13:23 +0400

    complete 36 lesson

--- Ch06/06_01/start/src/components/AddDayForm.js
+++ Ch06/06_01/start/src/components/AddDayForm.js
@@ -1,28 +1,45 @@
 import { PropTypes, Component } from 'react'
 
 export class AddDayForm extends Component {
-	render() {
 
+	constructor(props){
+		super(props)
+		this.submit = this.submit.bind(this)
+	}
+
+	submit(e){
+		e.preventDefault()
+		console.log("resort", this.refs.resort.value)
+        console.log("date", this.refs.date.value)
+        console.log("powder", this.refs.powder.checked)
+        console.log("backcountry", this.refs.backcountry.checked)
+	}
+
+	render() {
 		const { resort, date, powder, backcountry } = this.props
 		return(
-            <form className="add-day-form">
+            <form className="add-day-form" onSubmit={this.submit}>
 				<label htmlFor="resort">Resort name</label>
 				<input id="resort"
 					   type="text"
 					   required
-					   defaultValue={resort}/>
+					   defaultValue={resort}
+					   ref="resort"
+				/>
 
                 <label htmlFor="date">date</label>
                 <input id="date"
 					   type="date"
 					   required
                        defaultValue={date}
+                       ref="date"
 				/>
 
 				<div>
                 <input id="powder"
 					   type="checkbox"
                        defaultChecked={powder}
+                       ref="powder"
 				/>
                 <label htmlFor="powder">Powder day</label>
 				</div>
@@ -30,9 +47,13 @@ export class AddDayForm extends Component {
                 <input id="backcountry"
 					   type="checkbox"
                        defaultChecked={backcountry}
+                       ref="backcountry"
 				/>
 					<label htmlFor="backcountry">Backcountry date</label>
 				</div>
+				<button>
+					Add Button
+				</button>
 			</form>
 		)
 	}
