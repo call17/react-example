9d92b8a12d4047cbe1255515202c60488d30752a
Якимов Виктор Игоревич <VIYakimov.SBT@sberbank.ru>
2018-08-02 14:30:51 +0400

    complete 37 lesson

--- Ch06/06_01/start/src/components/AddDayForm.js
+++ Ch06/06_01/start/src/components/AddDayForm.js
@@ -1,5 +1,57 @@
 import { PropTypes, Component } from 'react'
 
+export const AddDayFrom = ({ resort, date, powder, backcountry }) => {
+
+	let _resort, _date, _powder, _backcountry
+    const submit = (e) => {
+        e.preventDefault()
+        console.log("resort", _resort.value)
+        console.log("date", _date.value)
+        console.log("powder", _powder.checked)
+        console.log("backcountry", _backcountry.checked)
+    }
+
+    return(
+        <form className="add-day-form" onSubmit={submit}>
+            <label htmlFor="resort">Resort name</label>
+            <input id="resort"
+                   type="text"
+                   required
+                   defaultValue={resort}
+                   ref={input => _resort =  input}
+            />
+
+            <label htmlFor="date">date</label>
+            <input id="date"
+                   type="date"
+                   required
+                   defaultValue={date}
+                   ref={input => _date =  input}
+            />
+
+            <div>
+                <input id="powder"
+                       type="checkbox"
+                       defaultChecked={powder}
+                       ref={input => _powder =  input}
+                />
+                <label htmlFor="powder">Powder day</label>
+            </div>
+            <div>
+                <input id="backcountry"
+                       type="checkbox"
+                       defaultChecked={backcountry}
+                       ref={input => _backcountry =  input}
+                />
+                <label htmlFor="backcountry">Backcountry date</label>
+            </div>
+            <button>
+                Add Button
+            </button>
+        </form>
+    )
+}
+
 export class AddDayForm extends Component {
 
 	constructor(props){
