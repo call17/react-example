13022d38a6a4fb8fd482dda7208d23ee8f5a33aa
Якимов Виктор Игоревич <VIYakimov.SBT@sberbank.ru>
2018-08-02 13:41:24 +0400

    complete 35 lesson

--- Ch06/06_01/start/src/components/AddDayForm.js
+++ Ch06/06_01/start/src/components/AddDayForm.js
@@ -1,8 +1,49 @@
-import { PropTypes } from 'react'
+import { PropTypes, Component } from 'react'
 
-export const AddDayForm = () => (
-	<h1>Add A Day</h1>
-)
+export class AddDayForm extends Component {
+	render() {
+
+		const { resort, date, powder, backcountry } = this.props
+		return(
+            <form className="add-day-form">
+				<label htmlFor="resort">Resort name</label>
+				<input id="resort"
+					   type="text"
+					   required
+					   defaultValue={resort}/>
+
+                <label htmlFor="date">date</label>
+                <input id="date"
+					   type="date"
+					   required
+                       defaultValue={date}
+				/>
+
+				<div>
+                <input id="powder"
+					   type="checkbox"
+                       defaultChecked={powder}
+				/>
+                <label htmlFor="powder">Powder day</label>
+				</div>
+                <div>
+                <input id="backcountry"
+					   type="checkbox"
+                       defaultChecked={backcountry}
+				/>
+					<label htmlFor="backcountry">Backcountry date</label>
+				</div>
+			</form>
+		)
+	}
+}
+
+AddDayForm.defaultProps = {
+    resort: "Kirkwood",
+    date: "2017-02-12",
+    powder: true,
+    backcountry: false
+}
 
 AddDayForm.propTypes = {
 	resort: PropTypes.string.isRequired,
--- Ch06/06_01/start/webpack.config.js
+++ Ch06/06_01/start/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 	devServer: {
 		inline: true,
 		contentBase: './dist',
-		port: 3000
+		port: 3001
 	},
 	module: {
 		loaders: [
