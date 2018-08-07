417e23452c5c3064e7b7b69afcc581b052b6d854
Якимов Виктор Игоревич <VIYakimov.SBT@sberbank.ru>
2018-08-07 13:12:42 +0400

    complete 40 lesson

--- Ch07/07_01/start/src/components/ui/Member.js
+++ Ch07/07_01/start/src/components/ui/Member.js
@@ -1,20 +1,22 @@
 import FaShield from 'react-icons/lib/fa/shield' 
 import { Component } from 'react'
 
-class Member extends Component {
+export class Member extends Component {
 
-render() {
-	
-    return (
-        <div className="member">
+    constructor(props){
+        super(props)
+    }
 
-        //
-        // TODO: Create Member Component
-        //
-
-        </div>
-    )
-}
-}
+    render() {
+        return (
+            <div className="member">
+                <h1>{this.props.name} {this.props.admin ? <FaShield/> :  null}</h1>
+                <a onClick={this.props.makeAdmin}>Make ADMIN</a>
+                <img src={this.props.thumbnail} alt="profile picture"/>
+                <p><a href={"mailto:" + this.props.email}>{this.props.email}</a></p>
+            </div>
+        )
+    }
+    }
 
 export default Member
